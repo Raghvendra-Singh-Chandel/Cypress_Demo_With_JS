c51b2048e2641a0c64fc40bde42aa8fe7cdc1244
= <=>
2023-08-01 12:48:28 +0530

    order created successfully

--- cypress/e2e/createOrder.cy.js
+++ cypress/e2e/createOrder.cy.js
@@ -2,10 +2,14 @@ describe("create order end to end scenario",()=> {
     beforeEach("login",()=> {
         cy.loginWithvalidCredentials()
     })
+    /**
+     * End To End Scenario: Placed Order
+     */
 
-    it.only ("create order",()=> {
+    it("create order",()=> {
         cy.selectProductToBuy()
         cy.placeOrder()
         cy.fillAllDetailsToProcessedPaymentPage()
+        cy.fillPaymentDetailsAndCompletePayment()
     })
 })
